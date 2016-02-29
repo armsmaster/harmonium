81d985cf6fa221883cd45182b4d45210c874ab02
Edward Gordin <edward.gordin@gmail.com>
2016-02-29 22:55:12 +0300

    lang1

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,7 +123,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'en'
+#LANGUAGE_CODE = 'en'
 
 # TIME_ZONE = 'UTC'
 TIME_ZONE = 'Europe/Moscow'
